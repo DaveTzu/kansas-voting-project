2970280fa01a9b06930e8c3e6b872572bf995998
Dave <DisembodiedMinstrel@gmail.com>
2023-04-24 11:01:21 -0500

    added some app.gets to view data from civic api

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -12,6 +13,41 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/electionQuery', async (req, res) => {
+  try {
+    const response = await axios.get('https://www.googleapis.com/civicinfo/v2/elections', {
+      params: {
+        key: process.env.GOOGLE_CIVIC_API_KEY
+      }
+    });
+
+    const electionData = response.data;
+    res.json(electionData);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching election data' });
+  }
+});
+
+app.get('/voterInfoQuery', async (req, res) => {
+  const electionId = 2000;
+  const address = '1701 HORACE ST NEW ORLEANS LA'; // Replace with user input or a latitude and longitude coordinate
+  
+  try {
+    const response = await axios.get('https://www.googleapis.com/civicinfo/v2/voterinfo', {
+      params: {
+        key: process.env.GOOGLE_CIVIC_API_KEY,
+        electionId: electionId,
+        address: address,
+      },
+    });
+
+    const voterInfoData = response.data;
+    res.json(voterInfoData);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching voter information' });
+  }
 });
 
 app.listen(port, () => {
